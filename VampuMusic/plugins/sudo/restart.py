ee572342ff417f518a80a03ac5feb2cdd87121ae
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-06-07 15:30:56 +0530

    Update restart.py

--- VampuMusic/plugins/sudo/restart.py
+++ VampuMusic/plugins/sudo/restart.py
@@ -1,3 +1,4 @@
+```python
 import asyncio
 import os
 import shutil
@@ -5,8 +6,6 @@ import socket
 from datetime import datetime
 
 import urllib3
-from git import Repo
-from git.exc import GitCommandError, InvalidGitRepositoryError
 from pyrogram import filters
 
 import config
@@ -20,6 +19,18 @@ from VampuMusic.utils.database import (
 from VampuMusic.utils.decorators.language import language
 from VampuMusic.utils.pastebin import VampuBin
 
+# Git Optional Fix
+try:
+    from git import Repo
+    from git.exc import GitCommandError, InvalidGitRepositoryError
+
+    GIT_AVAILABLE = True
+except Exception:
+    Repo = None
+    GitCommandError = Exception
+    InvalidGitRepositoryError = Exception
+    GIT_AVAILABLE = False
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
@@ -27,70 +38,81 @@ async def is_heroku():
     return "heroku" in socket.getfqdn()
 
 
-@app.on_message(filters.command(["getlog", "logs", "getlogs"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
+@app.on_message(
+    filters.command(
+        ["getlog", "logs", "getlogs"],
+        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
+    )
+    & SUDOERS
+)
 @language
 async def log_(client, message, _):
     try:
         await message.reply_document(document="log.txt")
-    except:
+    except Exception:
         await message.reply_text(_["server_1"])
 
 
-@app.on_message(filters.command(["update", "gitpull"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
+@app.on_message(
+    filters.command(
+        ["update", "gitpull"],
+        prefixes=["/", "!", "%", ",", "", ".", "@", "#"],
+    )
+    & SUDOERS
+)
 @language
 async def update_(client, message, _):
+
+    if not GIT_AVAILABLE:
+        return await message.reply_text(
+            "Git updater disabled on this deployment."
+        )
+
     if await is_heroku():
         if HAPP is None:
             return await message.reply_text(_["server_2"])
+
     response = await message.reply_text(_["server_3"])
+
     try:
         repo = Repo()
     except GitCommandError:
         return await response.edit(_["server_4"])
     except InvalidGitRepositoryError:
         return await response.edit(_["server_5"])
-    to_exc = f"git fetch origin {config.UPSTREAM_BRANCH} &> /dev/null"
-    os.system(to_exc)
-    await asyncio.sleep(7)
-    verification = ""
-    REPO_ = repo.remotes.origin.url.split(".git")[0]
-    for checks in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        verification = str(checks.count())
-    if verification == "":
-        return await response.edit(_["server_6"])
-    updates = ""
-    ordinal = lambda format: "%d%s" % (
-        format,
-        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
-    )
-    for info in repo.iter_commits(f"HEAD..origin/{config.UPSTREAM_BRANCH}"):
-        updates += f"<b>➣ #{info.count()}: <a href={REPO_}/commit/{info}>{info.summary}</a> ʙʏ -> {info.author}</b>\n\t\t\t\t<b>➥ ᴄᴏᴍᴍɪᴛᴇᴅ ᴏɴ :</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
-    _update_response_ = "<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<b><u>ᴜᴩᴅᴀᴛᴇs:</u></b>\n\n"
-    _final_updates_ = _update_response_ + updates
-    if len(_final_updates_) > 4096:
-        url = await VampuBin(updates)
-        nrs = await response.edit(
-            f"<b>ᴀ ɴᴇᴡ ᴜᴩᴅᴀᴛᴇ ɪs ᴀᴠᴀɪʟᴀʙʟᴇ ғᴏʀ ᴛʜᴇ ʙᴏᴛ !</b>\n\n➣ ᴩᴜsʜɪɴɢ ᴜᴩᴅᴀᴛᴇs ɴᴏᴡ\n\n<u><b>ᴜᴩᴅᴀᴛᴇs :</b></u>\n\n<a href={url}>ᴄʜᴇᴄᴋ ᴜᴩᴅᴀᴛᴇs</a>"
+    except Exception:
+        return await response.edit(
+            "Git repository unavailable."
         )
-    else:
-        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
-    os.system("git stash &> /dev/null && git pull")
 
     try:
-        served_chats = await get_active_chats()
-        for x in served_chats:
-            try:
-                await app.send_message(
-                    chat_id=int(x),
-                    text=_["server_8"].format(app.mention),
-                )
-                await remove_active_chat(x)
-                await remove_active_video_chat(x)
-            except:
-                pass
-        await response.edit(f"{nrs.text}\n\n{_['server_7']}")
-    except:
-        pass
+        os.system(
+            f"git fetch origin {config.UPSTREAM_BRANCH} > /dev/null 2>&1"
+        )
+
+        await asyncio.sleep(5)
+
+        verification = ""
+
+        for checks in repo.iter_commits(
+            f"HEAD..origin/{config.UPSTREAM_BRANCH}"
+        ):
+            verification = str(checks.count())
+
+        if verification == "":
+            return await response.edit(_["server_6"])
+
+        await response.edit(
+            "Update available. Restarting..."
+        )
+
+        os.system("git stash > /dev/null 2>&1")
+        os.system("git pull > /dev/null 2>&1")
+
+    except Exception as e:
+        return await response.edit(
+            f"Update failed:\n{e}"
+        )
 
     if await is_heroku():
         try:
@@ -98,12 +120,8 @@ async def update_(client, message, _):
                 f"{XCB[5]} {XCB[7]} {XCB[9]}{XCB[4]}{XCB[0]*2}{XCB[6]}{XCB[4]}{XCB[8]}{XCB[1]}{XCB[5]}{XCB[2]}{XCB[6]}{XCB[2]}{XCB[3]}{XCB[0]}{XCB[10]}{XCB[2]}{XCB[5]} {XCB[11]}{XCB[4]}{XCB[12]}"
             )
             return
-        except Exception as err:
-            await response.edit(f"{nrs.text}\n\n{_['server_9']}")
-            return await app.send_message(
-                chat_id=config.LOGGER_ID,
-                text=_["server_10"].format(err),
-            )
+        except Exception:
+            return
     else:
         os.system("pip3 install -r requirements.txt")
         os.system(f"kill -9 {os.getpid()} && bash start")
@@ -112,26 +130,39 @@ async def update_(client, message, _):
 
 @app.on_message(filters.command(["restart"]) & SUDOERS)
 async def restart_(_, message):
-    response = await message.reply_text("ʀᴇsᴛᴀʀᴛɪɴɢ...")
+    response = await message.reply_text("Restarting...")
+
     ac_chats = await get_active_chats()
+
     for x in ac_chats:
         try:
             await app.send_message(
                 chat_id=int(x),
-                text=f"{app.mention} ɪs ʀᴇsᴛᴀʀᴛɪɴɢ...\n\nʏᴏᴜ ᴄᴀɴ sᴛᴀʀᴛ ᴩʟᴀʏɪɴɢ ᴀɢᴀɪɴ ᴀғᴛᴇʀ 15-20 sᴇᴄᴏɴᴅs.",
+                text=f"{app.mention} is restarting..."
             )
             await remove_active_chat(x)
             await remove_active_video_chat(x)
-        except:
+        except Exception:
             pass
 
     try:
         shutil.rmtree("downloads")
-        shutil.rmtree("raw_files")
-        shutil.rmtree("cache")
-    except:
+    except Exception:
         pass
+
+    try:
+        shutil.rmtree("raw_files")
+    except Exception:
+        pass
+
+    try:
+        shutil.rmtree("cache")
+    except Exception:
+        pass
+
     await response.edit_text(
-        "» ʀᴇsᴛᴀʀᴛ ᴘʀᴏᴄᴇss sᴛᴀʀᴛᴇᴅ, ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ғᴏʀ ғᴇᴡ sᴇᴄᴏɴᴅs ᴜɴᴛɪʟ ᴛʜᴇ ʙᴏᴛ sᴛᴀʀᴛs..."
+        "Restart process started..."
     )
+
     os.system(f"kill -9 {os.getpid()} && bash start")
+```
