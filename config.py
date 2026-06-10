6a1772dc3f03cfec34afc6362d5ba809394bdfe1
ᯓ꯭𓆰꯭×꯭🔥⁠⁠⁠𝐕꯭𝐀꯭𝐌꯭𝐏꯭𝐈꯭𝐑꯭𝐄꯭ 𝐊꯭𝐈꯭𝐍꯭𝐆 ꯭🌾꯭ <mdkhateeb07302@gmail.com>
2026-06-10 21:47:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Vars For API End Pont.
-YTPROXY_URL = getenv("YTPROXY_URL", 'https://teaminflex.xyz') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , 'INFLEX77137628D') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://t.me/NickApiBot') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , 'NICK_OWNER_60ZL') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 # ======================================================
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
